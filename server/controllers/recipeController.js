2b921f880eb5bfa7541e45dea13f5bf6eb82d23a
thepritrax <pritrax@gmail.com>
2022-05-17 00:07:09 +0530

    fixed controller and categories breadcrumb

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -9,14 +9,7 @@ exports.homepage = async(req, res) => {
   try {
     const limitNumber = 5;
     const categories = await Category.find({}).limit(limitNumber);
-    const latest = await Recipe.find({}).sort({_id: -1}).limit(limitNumber);
-    const thai = await Recipe.find({ 'category': 'Thai' }).limit(limitNumber);
-    const american = await Recipe.find({ 'category': 'American' }).limit(limitNumber);
-    const chinese = await Recipe.find({ 'category': 'Chinese' }).limit(limitNumber);
-
-    const food = { latest, thai, american, chinese };
-
-    res.render('index', { title: 'Cooking Blog - Home', categories, food } );
+    res.render('index', { title: 'Cooking Blog - Home', categories } );
   } catch (error) {
     res.satus(500).send({message: error.message || "Error Occured" });
   }
@@ -30,7 +23,7 @@ exports.exploreCategories = async(req, res) => {
   try {
     const limitNumber = 20;
     const categories = await Category.find({}).limit(limitNumber);
-    res.render('categories', { title: 'Cooking Blog - Categoreis', categories } );
+    res.render('categories', { title: 'Cooking Blog - Categories', categories } );
   } catch (error) {
     res.satus(500).send({message: error.message || "Error Occured" });
   }

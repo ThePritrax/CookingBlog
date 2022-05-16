956935531e88c4536335433c77b8f8ca2eccca83
thepritrax <pritrax@gmail.com>
2022-05-16 23:33:48 +0530

    fetched and updated category cards from database

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -6,40 +6,29 @@ const Category = require('../models/Category');
  * Homepage
  */
 exports.homepage = async(req, res) => {
-    res.render('index',{ title: 'Cooking Blog - Home' });
+    try {
+        const limitNumber = 5;
+        const categories = await Category.find({}).limit(limitNumber);
+
+        res.render('index',{ title: 'Cooking Blog - Home', categories });
+    } catch(error) {
+        res.status(500).send({message: error.message || "Error Occured"});
+    }
+
 }
 
-async function insertDummyCategoryData(){
-  try {
-    await Category.insertMany([
-      {
-        "name": "Thai",
-        "image": "thai-food.jpg"
-      },
-      {
-        "name": "American",
-        "image": "american-food.jpg"
-      }, 
-      {
-        "name": "Chinese",
-        "image": "chinese-food.jpg"
-      },
-      {
-        "name": "Mexican",
-        "image": "mexican-food.jpg"
-      }, 
-      {
-        "name": "Indian",
-        "image": "indian-food.jpg"
-      },
-      {
-        "name": "Spanish",
-        "image": "spanish-food.jpg"
-      }
-    ]);
-  } catch (error) {
-    console.log('err', + error)
+/**
+ * GET /categories
+ * Categories 
+*/
+exports.exploreCategories = async(req, res) => {
+    try {
+      const limitNumber = 20;
+      const categories = await Category.find({}).limit(limitNumber);
+      res.render('categories', { title: 'Cooking Blog - Categoreis', categories } );
+    } catch (error) {
+      res.satus(500).send({message: error.message || "Error Occured" });
+    }
   }
-}
 
-insertDummyCategoryData();
+

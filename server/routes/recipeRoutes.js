e40f8cd7476614231bd6afa2db70d64dbb9181e8
thepritrax <pritrax@gmail.com>
2022-06-19 14:00:17 +0530

    added search, latest and random functionalities

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -9,5 +9,9 @@ router.get('/', recipeController.homepage);
 router.get('/categories', recipeController.exploreCategories);
 router.get('/recipe/:id', recipeController.exploreRecipe);
 router.get('/categories/:id', recipeController.exploreCategoriesById);
+router.post('/search', recipeController.searchRecipe);
+router.get('/explore-latest', recipeController.exploreLatest);
+router.get('/explore-random', recipeController.exploreRandom);
+
 
 module.exports = router;
